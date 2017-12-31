29d1c22fbac023f7ba3cd21f358004a9c887701a
José Antonio Pérez Pérez <josiete007@gmail.com>
2017-12-31 17:05:41 +0100

    Fixed some PEP8 errors

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import random
-import string
+
 
 class Dummy:
 
@@ -15,7 +15,7 @@ class Dummy:
             surname = self.parents[0]
         else:
             surname = getSurname()
-        self.name =  getName(self.gender) +" "+ surname
+        self.name = getName(self.gender) + " " + surname
         # Concepto de generacion
 
     def getMarriage(self, dummy):
@@ -51,12 +51,13 @@ class DummyList:
                         gender: {gender}
                         age: {age}
                         money: {money}
-                        marriaged_with: {marriaged}""".format(name=dummy.name,
-                                                             gender=dummy.gender,
-                                                             age=dummy.age,
-                                                             money=dummy.money,
-                                                             marriaged=dummy.marriaged,
-                                                             )
+                        marriaged_with: {marriaged}
+                        """.format(name=dummy.name,
+                                   gender=dummy.gender,
+                                   age=dummy.age,
+                                   money=dummy.money,
+                                   marriaged=dummy.marriaged,
+                                   )
 
     def terminate(self, dummy):
         """ kills a dummy. Not a murder, Just a natural die"""
@@ -67,20 +68,22 @@ class DummyList:
         """ create a generation of n individuos"""
         for i in range(n):
             self.stack.append(Dummy())
-        self.generation += 1 
-    
+        self.generation += 1
+ 
     def next_year(self):
-        """ Displace time one year forward. It means all created dummies increase his age one year."""
-        self.year +=1 
+        """ Displace time one year forward.
+        It means all created dummies increase his age one year."""
+        self.year += 1 
         for dummy in self.stack:
             dummy.age += 1
-    
+ 
     def find_wife(self):
-        """ Dummies can get marriage. This method try to find a partner to a male dummy. """
+        """ Dummies can get marriage.
+            This method try to find a partner to a male dummy. """
         for male in self._get_males():
             for female in self._get_females():
                 try:
-                    if 3 > random.randint(1,10):
+                    if 3 > random.randint(1, 10):
                         male.getMarriage(female)
                 except ValueError:
                     pass
@@ -91,7 +94,7 @@ class DummyList:
 
     def set_babies(self):
         for couple in self.get_couples():
-            if 2 > random.randint(1,10):
+            if 2 > random.randint(1, 10):
                 baby = Dummy()
                 baby.parents = couple
                 self.add(baby)
@@ -101,26 +104,39 @@ class DummyList:
 
     def _get_females(self):
         return [dummy for dummy in self.stack if dummy.gender == 'female' and dummy.marriaged is None]
-    
+ 
     def _get_males(self):
         return [dummy for dummy in self.stack if dummy.gender == 'male' and dummy.marriaged is None]    
 
 def getSurname():
 
     lista_apellidos = ['Alvarez', 'Lopez', 'Martinez', 'Perez', 'Fernandez', 'Garcia', 'Sanchez', 'Rodriguez', 'Diaz', 'Sanz', 'Castellanos', 'Triguero', 'Gago', 'Ramos']     
-    return lista_apellidos[random.randint(1, len(lista_apellidos)) -1]
+    return lista_apellidos[random.randint(1, len(lista_apellidos)) - 1]
 
 def getName(gender):
 
-    lista_nombres_masculinos = ['Daniel', 'Mario', 'Rodrigo', 'Jose', 'Ronaldo', 'Tom', 'Carlos', 'Julio', 'Alberto', 'Ricardo', 'Fernando', 'Roberto', 'Alfonso', 'Alonso']
+    lista_nombres_masculinos = ['Daniel',
+                                'Mario',
+                                'Rodrigo',
+                                'Jose',
+                                'Ronaldo',
+                                'Tom',
+                                'Carlos',
+                                'Julio',
+                                'Alberto',
+                                'Ricardo',
+                                'Fernando',
+                                'Roberto',
+                                'Alfonso',
+                                'Alonso']
     lista_nombres_femeninos = ['Monica', 'Rosa', 'Juana', 'Julia', 'Adela', 'Sara', 'Carmen', 'Ana', 'Imelda', 'Evelia']
 
     if gender is 'male':
-        return lista_nombres_masculinos[random.randint(1, len(lista_nombres_masculinos)) -1]
+        return lista_nombres_masculinos[random.randint(1, len(lista_nombres_masculinos)) - 1]
     else: 
-        return lista_nombres_femeninos[random.randint(1, len(lista_nombres_femeninos)) -1]
+        return lista_nombres_femeninos[random.randint(1, len(lista_nombres_femeninos)) - 1]
+
 
 def getRandomGender():
     genders = ['male', 'female']
-    return genders[random.randint(0,1)]
-
+    return genders[random.randint(0, 1)]
